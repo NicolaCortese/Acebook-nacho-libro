58c01f1f9dc0c0bdadb523d35bf37c956ac07541
Maria Gromovaja <72412305+ruiined@users.noreply.github.com>
2022-04-12 16:47:01 +0100

    Delete db.js

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://nacholibro:makers2022@cluster0.njrw6.mongodb.net/aceBook?retryWrites=true&w=majority');
-
-// const Cat = mongoose.model('Cat', { name: String });
-
-// const kitty = new Cat({ name: 'Zildjian' });
-// kitty.save().then(() => console.log('meow'));
-console.log("hi")
