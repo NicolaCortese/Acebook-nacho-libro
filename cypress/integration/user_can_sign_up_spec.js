664b88120a1f9e7d065809efa18f39cfbc73477f
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-13 11:53:00 +0100

    Testing the registration input

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,20 +1,36 @@
-describe("Registration", () => {
-  it("A user signs up and is redirected to sign in", () => {
+// const User = require('../../models/user');
+
+describe('Registration', () => {
+  // beforeEach(() => {
+  //   await User.deleteMany();
+  // })
+
+  it('A user signs up and is redirected to sign in', () => {
     // sign up
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
+    cy.visit('/users/new');
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').type('password');
+    cy.get('#submit').click();
 
-    cy.url().should("include", "/sessions/new");
-    
+    cy.url().should('include', '/sessions/new');
+
     // sign up from the homepage
-    cy.visit("/");
-    cy.get("#signup").click();
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
+    cy.visit('/');
+    cy.get('#signup').click();
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').type('password');
+    cy.get('#submit').click();
 
-    cy.url().should("include", "/sessions/new");
+    cy.url().should('include', '/sessions/new');
+  });
+
+  it('Throws an error when email already exists', () => {
+    cy.visit('/users/new');
+    cy.get('#email').type('someone@example.com');
+    cy.get('#password').type('password');
+    cy.get('#submit').click();
+
+    cy.url().should('include', './users/new');
+    cy.should('include', 'User already exists');
   });
 });
