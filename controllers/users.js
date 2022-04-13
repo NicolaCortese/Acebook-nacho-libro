66978c31265db3748308c8cd85f01950cd1c7b77
Samuel Raducan <samuel.thembp@gmail.com>
2022-04-13 17:42:51 +0100

    Add user registration
    Co-authored-by: Nicola Cortese <NicolaCortese@users.noreply.github.com>

--- controllers/users.js
+++ controllers/users.js
@@ -1,27 +1,37 @@
-const User = require("../models/user");
+const User = require('../models/user');
 
 const UsersController = {
   New: (req, res) => {
-    res.render("users/new", {});
+    res.render('users/new', {});
   },
 
   Create: async (req, res) => {
-    console.log("creating a new user");
-    console.log(req.body);
+    // console.log(req.body);
     const user = new User(req.body);
-    const duplicate = await User.exists({ email: req.body.email });
 
-    if (duplicate) {
-      console.log("this email already exists");
-      res.redirect("/users/new");
+    // Return when the fields are empty
+    if (user.username === '' || user.email === '' || user.password === '') {
+      console.log('Enter the name or password.');
+      res.redirect('/users/new');
     } else {
-      // console.log(user);
-      user.save((err) => {
-        if (err) {
-          throw err;
-        }
-        res.status(201).redirect("/posts");
+      const userAlreadyExists = await User.find({
+        $or: [{ email: req.body.email }, { username: req.body.username }],
       });
+
+      console.log(userAlreadyExists);
+      if (userAlreadyExists) {
+        console.log('This user already exists');
+        res.redirect('/users/new');
+      } else {
+        console.log('creating a new user');
+        console.log(`User information: ${user}`);
+        user.save((err) => {
+          if (err) {
+            throw err;
+          }
+          res.status(201).redirect('/posts');
+        });
+      }
     }
   },
 };
