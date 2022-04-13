fe7f1c6708d04247d711ef0ce217340f1427bacd
Nicola Cortese <nicolacortese92@gmail.com>
2022-04-13 11:51:30 +0100

    checking if the email already exists in the database

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -1,13 +1,14 @@
-describe("Registration", () => {
-  it("A user signs up and is redirected to sign in", () => {
-    // sign up
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
+const mongoose = require("mongoose");
 
-    cy.url().should("include", "/sessions/new");
-    
+// require("../../spec/mongodb_helper");
+
+
+describe("Registration", () => {
+  before( async () => {
+    await mongoose.connection.collections.users.drop()
+  });
+
+  it("A user signs up and is redirected to sign in", () => {
     // sign up from the homepage
     cy.visit("/");
     cy.get("#signup").click();
@@ -16,5 +17,20 @@ describe("Registration", () => {
     cy.get("#submit").click();
 
     cy.url().should("include", "/sessions/new");
+  });
+
+  it("will not sign up if the email already exists", () => {
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@example.com");
+    cy.get("#password").type("password");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/users/new");
+    //add an error message here
   });
 });
