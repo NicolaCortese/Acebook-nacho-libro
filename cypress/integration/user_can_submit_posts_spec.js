433f63366a932c00a4b15274b5851b4904f96b6e
Joe Thornton <thornton.jp@gmail.com>
2022-04-14 15:45:04 +0100

    Amended feature test for user submitting a post
    Included username on the sign-up page
    
    Co-authored-by: Felix Taljaard <felixtaljaard@users.noreply.github.com>

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -2,13 +2,14 @@ describe("Timeline", () => {
   it("can submit posts, when signed in, and view them", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("batman@example.com");
     cy.get("#password").type("password");
+    cy.get("#username").type("batman");
     cy.get("#submit").click();
 
     // sign in
     cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("batman@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
 
