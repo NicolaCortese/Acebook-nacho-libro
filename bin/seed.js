64983e3c7da3c7693b216dfce50b7cf4d81f95c3
Joe Thornton <thornton.jp@gmail.com>
2022-04-15 13:24:10 +0100

    seed.js is clears the database and adds a single user

--- bin/seed.js
+++ bin/seed.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
-// const User = require("../models/user");
+const User = require("../models/user");
 
 const clearDB = async () => {
   await mongoose.connect("mongodb://127.0.0.1/acebook_test", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  await mongoose.connection.collections.users.drop();
+  // await mongoose.connection.collections.users.drop();
+
+  // await User.deleteMany({});
+
+  const user = await new User({
+    email: "batman@example.com",
+    password: "password",
+    username: "batman",
+  });
+
+  await user.save();
+
   await mongoose.connection.close();
 };
 
