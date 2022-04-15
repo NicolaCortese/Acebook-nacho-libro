13b54eef0ae9bb7233a262155d2ae50b43864b73
ruiined <ruiined@gmail.com>
2022-04-15 16:37:30 +0100

    Fixed the db connection

--- cypress/integration/nav_bar_spec.js
+++ cypress/integration/nav_bar_spec.js
@@ -1,18 +1,19 @@
 describe("Nav bar", () => {
   describe("When not signed in", () => {
-    it("checks it contains all nav elements but no logout", () => {
+    it("checks it contains all nav elements if not signed in", () => {
       cy.visit("/");
       cy.get("#li-sign-in").should("contain", "Sign in");
       cy.get("#li-sign-up").should("contain", "Sign up");
     });
   });
   describe("When signed in", () => {
-    it("checks it contains all nav elements but no logout", () => {
+    it("checks it contains all nav elements when signed in", () => {
       // sign up
-      // cy.visit("/users/new");
-      // cy.get("#email").type("someone@example.com");
-      // cy.get("#password").type("password");
-      // cy.get("#submit").click();
+      cy.visit("/users/new");
+      cy.get("#email").type("someone@example.com");
+      cy.get('#username').type('someone')
+      cy.get("#password").type("password");
+      cy.get("#submit").click();
 
       // sign in
       cy.visit("/sessions/new");
