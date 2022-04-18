b33929fdd45923aeba0bb6f21da3cf5e0fc972e5
Felix Taljaard <felixtaljaard@hotmail.com>
2022-04-18 15:11:27 +0100

    Tests are failing and dont know why

--- cypress/integration/user_can_see_their_profile_page_spec.js
+++ cypress/integration/user_can_see_their_profile_page_spec.js
@@ -1,34 +1,27 @@
 describe("Timeline", () => {
   it("can submit posts, when signed in, and view them", () => {
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
-    cy.get('#username').type('someone')
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-
     // sign in
     cy.visit("/sessions/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("batman@example.com");
     cy.get("#password").type("password");
     cy.get("#submit").click();
     // submit a post
     cy.visit("/posts");
     cy.contains("New post").click();
-    cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
+    cy.get("#new-post-form").find('#message').type("Batman post");
     cy.get("#new-post-form").submit();
-    // cy.get(".posts").should("contain", "Hello, world!");
-    // go to profile page and show posts by user
     cy.visit("/users/profile");
-    // cy.contains("New post").click();
-    // cy.get("#new-post-form").find('[type="text"]').type("Hello, world!");
-    // cy.get("#new-post-form").submit();
-    cy.get(".posts").should("contain", "Hello, world!");
-  });
+    cy.get(".posts").should(
+      "contain",
+      "Batman post",
+    );
+    // 
+    });
 
-  it("can submit posts, when signed in, and view them", () => {
-    cy.visit("/sessions/new");
-    cy.get("#email").type("joker@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-  });
+  // it("can submit posts, when signed in, and view them", () => {
+  //   cy.visit("/sessions/new");
+  //   cy.get("#email").type("joker@example.com");
+  //   cy.get("#password").type("password");
+  //   cy.get("#submit").click();
+  // });
 });
