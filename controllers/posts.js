d9fb92a421bc822c5d3078f0382c1a7566a6c9db
ruiined <ruiined@gmail.com>
2022-04-18 17:14:17 +0100

    Implemented edit and delete

--- controllers/posts.js
+++ controllers/posts.js
@@ -24,6 +24,35 @@ const PostsController = {
       res.status(201).redirect("/posts");
     });
   },
+  Edit: (req, res) => {
+    const post_id = req.params.id;
+    Post.find({_id: post_id}, (err, post) => {
+      if (err) {
+        throw err;
+      }
+      console.log("post info:")
+      console.log(post.message)
+
+      res.render("posts/edit", { post: post });
+    });
+  },
+  Save: (req, res) => {
+    const post_id = req.params.id;
+    Post.find({_id: post_id}, (err, post) => {
+      if (err) {
+        throw err;
+      }
+
+      res.render("posts/edit", { post: post });
+    });
+  },
+  Delete: (req, res) => {
+    const post_id = req.params.id
+    console.log(post_id)
+    Post.deleteOne({ _id: post_id }, () => {
+      res.redirect("/posts");
+    });
+  },
   Like: (req, res) => {
     const post_id = req.body.post_id;
     const user = req.session.user;
