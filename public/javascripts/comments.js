4a9d6153fc549ad7030612425a5c064e06b6cca8
Felix Taljaard <felixtaljaard@hotmail.com>
2022-04-21 17:27:40 +0100

    It actually works

--- public/javascripts/comments.js
+++ public/javascripts/comments.js
@@ -2,7 +2,9 @@ const commentButton = document.querySelectorAll(".comment-button");
 
 commentButton.forEach((element) => {
   element.addEventListener("click", () => {
+    
     onCommentButtonClick(element);
+    
   });
 });
 
@@ -15,8 +17,11 @@ const onCommentButtonClick = (element) => {
       post_id: element.id.replace("comment-",""),
     })
     .then((response) => {
-      console.log(response.data.comments);
+      // console.log(response.data.comments);
+      removeComments(response.data);
+
       updateComments(response.data);
+      document.querySelector(`#text-${element.id}`).value = ""
     })
     .catch((error) => {
       console.log(error);
@@ -25,14 +30,43 @@ const onCommentButtonClick = (element) => {
 /* eslint-enable */
 
 const updateComments = (data) => {
-  let commentAuthorsEl = document.querySelector('.commented-by')
-
-  commentAuthorsEl.innerText = ''
+  let commentSectionEl = document.querySelector(`#comment-section-${data._id}`)
+  
+  
   data.comments.forEach(comment => {
-    console.log(comment)
+   
+    // console.log(comment._id)
+    let commentObject = document.createElement('div')
+    commentObject.className = `comment-object`
+    // commentObject.id = comment.id
+    commentSectionEl.append(commentObject)
+    console.log(comment._id)
+    
+    
     let name = document.createElement("p")
     name.className = "comment-author"
     name.innerText = comment.author
-    commentAuthorsEl.append(name)
+    commentObject.append(name)
+
+    let text = document.createElement("p")
+    text.className = "comment-text"
+    text.innerText = comment.message
+    commentObject.append(text)
+
+    let time = document.createElement("time")
+    time.className = "timeago"
+    // time.dateTime = comment.timestamp
+    time.innerText = moment().to(comment.timestamp)
+    commentObject.append(time)
+    
+
   })
 }
+
+const removeComments = (data) => {
+    console.log(data._id)
+    let section = document.querySelector(`#comment-section-${data._id}`)
+
+    section.innerHTML = ""
+}
+
