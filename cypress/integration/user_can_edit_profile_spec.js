719abd1c721f9a2e0fb758ccf6452a4fbdf09a02
Nicola Cortese <nicolacortese92@gmail.com>
2022-04-22 09:57:49 +0100

    fixed test of user can edit profile with correct id elements

--- cypress/integration/user_can_edit_profile_spec.js
+++ cypress/integration/user_can_edit_profile_spec.js
@@ -1,5 +1,5 @@
-describe('Update settings', () => {
-  it('redirects to the profile page', () => {
+describe("Update settings", () => {
+  it("redirects to the profile page", () => {
     // sign in
     cy.visit("/sessions/new");
     cy.get("#email").type("batman@example.com");
@@ -10,22 +10,23 @@ describe('Update settings', () => {
     cy.visit("/users/batman/settings");
 
     //fills in form
-    cy.get("#coverPhoto").type("https://picsum.photos/100/100");
     cy.get("#birthday").type("2000-01-01");
-    cy.get("#livesIn").type("Maracaibo");
-    cy.get("#worksAt").type("NASA");
+    cy.get("#lives-in").type("Maracaibo");
+    cy.get("#works-at").type("NASA");
     cy.get("#hobbies").type("Sailing, Bowling and Eating");
     cy.get("#submit").click();
 
     // Check that the details have been updated
     cy.url().should("include", "/users/batman/profile");
-    cy.get("#coverPhotoDiv").find("img").should("be.visible");
-    cy.get("#profilePicDiv").find("img").should("be.visible");
+    cy.get("#profile-details").find("#profile-pic").should("be.visible");
+    cy.get("#main-container").find("#cover-photo").should("be.visible");
 
-    cy.get("#birthday").should("contain", "01/01/200");
-    cy.get("#livesIn").should("contain", "Maracaibo");
-    cy.get("#worksAt").should("contain", "NASA");
-    cy.get("#hobbies").should("contain", "Sailing, Bowling and Eating");
-    cy.get(".editProfile").should("contain", "Edit Profile");
-  })
-})
+    cy.get("#profile-details")
+      .should("contain", "01/01/200")
+      .and("contain", "Maracaibo")
+      .and("contain", "NASA")
+      .and("contain", "Sailing, Bowling and Eating");
+
+    cy.get(".edit-profile").should("contain", "Edit Profile");
+  });
+});
